f8f8d949971a36a0cc1eb4b041aee911d606c830
Deji Salaam <dejisalaam@The-Hidden-Genius-Project-32.local>
2022-10-12 18:20:57 -0700

    first commit

---
+++ Terminal_lingo.py
@@ -0,0 +1,23 @@
+def terminal_command():
+   command = input("Type a command for a defition: ")
+   if (command == "touch"):
+       print("\n")
+       print("this command will change the directory")
+   if (command == "cd"):
+       print("\n")
+       print("Use this command after navigating into a directory")
+   if (command == "ls"):
+       print("\n")      
+       print("Use this command after navigating into a directory")
+   if (command == "mkdir"):
+       print("\n")
+       print("This command will allow you to create a directory (folder) right from the CLI.")
+   if (command == "cat"):
+       print("\n")
+       print("Prints contents of a file to terminal")
+terminal_command()
+
+
+
+
+	
